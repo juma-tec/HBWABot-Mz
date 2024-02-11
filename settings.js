8ed50556b416a92eac0ba658f69d8d799eb4c2b2
juma-tec <154720378+juma-tec@users.noreply.github.com>
2024-02-11 16:26:56 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,14 +8,14 @@ global.autoTyping = false // true a i dah chuan i type la a lang ang
 global.autoRecord = false // true a i dah chuan Voice record angin i lang ang
 global.available = true // false a i dah chuan offline angin a awm ang
 global.autoread = false // true a i dah chuan message zawng zawng read vek zelin a awm ang 
-global.autoread_status = false // true a i dah chuan auto status view in a awm ang
+global.autoread_status = true // true a i dah chuan auto status view in a awm ang
 
 //info
-global.owner = ['918416093656'] // I phone number in thlak rawh
-global.ownernumber = '918416093656' // I phone number in thlak rawh
-global.ownername = "꧁Herbert♕꧂" // I hming dah tel bawk la
+global.owner = ['254769073703'] // I phone number in thlak rawh
+global.ownernumber = '254 723 616685'// I phone number in thlak rawh
+global.ownername = "꧁jumatechn ♕꧂" // I hming dah tel bawk la
 global.location = "India, Mizoram, Aizawl" // I awmna hmun daha
-global.botname = "HBWABot Mizo" // I bot hming tur dah la
+global.botname = "figaninty8 jay" // I bot hming tur dah la
 global.ownerweb = "https://youtube.com/@HBMods_Channel"
 global.themeemoji = '🤖'
 global.creator = "918416093656@s.whatsapp.net" // @ symbol hmaa number hi i phone number in thlak rawh
